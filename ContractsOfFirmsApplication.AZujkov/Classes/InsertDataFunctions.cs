974041ecff808a31b20f1c19f64a2c21ce2edb71
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-10-31 20:29:29 +0300

    в классе InsertDataFunctions добавил в перегрузку метода ConnectToDataBase переменную changedRecordsCount

--- ContractsOfFirmsApplication.AZujkov/Classes/InsertDataFunctions.cs
+++ ContractsOfFirmsApplication.AZujkov/Classes/InsertDataFunctions.cs
@@ -10,39 +10,40 @@ namespace ContractsOfFirmsApplication.AZujkov.Classes
 {
     class InsertDataFunctions : ConnectionWork
     {
+        private byte changedRecordsCount = 1;
         public bool AddNewContract(string jurName)
         {
             bool flagresult = false;
             string query = string.Format("INSERT INTO dbo.Firms (JurName) VALUES ('{0}')", jurName);
-            ConnectToDataBase(query, flagresult);
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
         public bool AddNewPerson(string firstName, string secondName)
         {
             bool flagresult = false;
             string query = string.Format("INSERT INTO dbo.People (FirstName, SecondName) VALUES ('{0}', '{1}')", firstName, secondName);
-            ConnectToDataBase(query, flagresult);
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
         public bool AddNewUser(string firstName, string secondName)
         {
             bool flagresult = false;
             string query = string.Format("INSERT INTO dbo.Users (FirstName, LastName) VALUES ('{0}', '{1}')", firstName, secondName);
-            ConnectToDataBase(query, flagresult);
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
         public bool AddNewProperties(string name)
         {
             bool flagresult = false;
             string query = string.Format("INSERT INTO dbo.Properties (Name) VALUES ('{0}')", name);
-            ConnectToDataBase(query, flagresult);
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
         public bool AddNewOrder(UInt16 contractId, DateTime createDate, UInt16 userId)
         {
             bool flagresult = false;
             string query = string.Format("INSERT INTO dbo.Orders (ContractId, CreateDate, UserId) VALUES ('{0}', '{1}', '{2}')", contractId, createDate, userId);
-            ConnectToDataBase(query, flagresult);
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
     }

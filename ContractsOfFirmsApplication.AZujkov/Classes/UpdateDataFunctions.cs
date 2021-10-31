2c0e9442c4542d130ac9fc90f0bc38feb7adf3cc
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-10-31 21:21:38 +0300

    добавил в класс функцию для изменения записи в таблице Users

--- ContractsOfFirmsApplication.AZujkov/Classes/UpdateDataFunctions.cs
+++ ContractsOfFirmsApplication.AZujkov/Classes/UpdateDataFunctions.cs
@@ -8,6 +8,12 @@ namespace ContractsOfFirmsApplication.AZujkov.Classes
 {
     class UpdateDataFunctions : ConnectionWork
     {
-
+        byte changedRecordsCount = 1;
+        public bool UserUpdateRecordFunction(string query)
+        {
+            bool flagresult = false;
+            ConnectToDataBase(query, flagresult, changedRecordsCount);
+            return flagresult;
+        }
     }
 }

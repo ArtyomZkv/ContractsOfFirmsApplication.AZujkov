282f6c2f9edc1409429180bc9b9a1f4a1a37b916
Artyom Zuikov <zujkov.ks52@gmail.com>
2021-11-01 19:29:46 +0300

    пределал методы в классе UpdateDataFunction

--- ContractsOfFirmsApplication.AZujkov/Classes/UpdateDataFunctions.cs
+++ ContractsOfFirmsApplication.AZujkov/Classes/UpdateDataFunctions.cs
@@ -9,28 +9,25 @@ namespace ContractsOfFirmsApplication.AZujkov.Classes
     class UpdateDataFunctions : ConnectionWork
     {
         byte changedRecordsCount = 1;
-        public bool UserUpdateRecordFunction(string query)
+        public bool ZeroForeignKeyUpdateRecordFunction(string query)
         {
             bool flagresult = false;
             ConnectToDataBase(query, flagresult, changedRecordsCount);
             return flagresult;
         }
-        public bool FirmsUpdateRecordsFunction(string query)
+        public bool OneForeignKeyUpdateRecordFunction(string query, string query1)
         {
             bool flagresult = false;
             ConnectToDataBase(query, flagresult, changedRecordsCount);
+            ConnectToDataBase(query1, flagresult, changedRecordsCount);
             return flagresult;
         }
-        public bool PeopleUpdateRecordsFunction(string query)
+        public bool TwoForeignKeysUpdateRecordFunction(string query, string query1, string query2)
         {
             bool flagresult = false;
             ConnectToDataBase(query, flagresult, changedRecordsCount);
-            return flagresult;
-        }
-        public bool PropertiesUpdateRecordsFunction(string query)
-        {
-            bool flagresult = false;
-            ConnectToDataBase(query, flagresult, changedRecordsCount);
+            ConnectToDataBase(query1, flagresult, changedRecordsCount);
+            ConnectToDataBase(query2, flagresult, changedRecordsCount);
             return flagresult;
         }
     }
